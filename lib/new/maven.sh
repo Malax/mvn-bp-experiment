b0f6052715b7fcc59a716a5fdc0ce44cecbe4ac4
Manuel Fuchs <manuel.fuchs@salesforce.com>
2020-11-20 09:40:06 +0100

    WIP

--- lib/new/maven.sh
+++ lib/new/maven.sh
@@ -5,7 +5,7 @@ function detect_maven_version() {
 
 	local selected_version=""
 	if [[ -f "${system_properties_path}" ]]; then
-		selected_version=$(get_java_properties_value "maven.version" < "${system_properties_path}")
+		selected_version=$(get_java_properties_value "maven.version" <"${system_properties_path}")
 	fi
 
 	echo "${selected_version:-$default_version}"
@@ -21,4 +21,9 @@ function get_maven_tarball_url() {
 	maven_tarball_urls[4.0.0]="https://whatever.tar.gz"
 
 	echo "${maven_tarball_urls["${maven_version}"]}"
+}
+
+function has_maven_wrapper() {
+	local -r app_directory="${1:?}"
+	[[ -f "${app_directory}/mvnw" && -f "${app_directory}/.mvn/wrapper/maven-wrapper.properties" ]]
 }
