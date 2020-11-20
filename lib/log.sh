b50a6593eaa69382908a4dab3cdba719ee603cc6
Manuel Fuchs <manuel.fuchs@salesforce.com>
2020-11-20 15:29:37 +0100

    WIP

---
+++ lib/log.sh
@@ -0,0 +1,71 @@
+log::classic::error() {
+	# send all of our output to stderr
+	exec 1>&2
+
+	# If arguments are given, redirect them to stdin this allows the funtion to be invoked with a string argument,
+	# or with stdin, e.g. via <<-EOF
+	(($#)) && exec <<<"$@"
+
+	echo
+	echo -n " !     ERROR: $(cat -)" | log::indent no_first_line_indent
+	echo
+}
+
+log::classic::warning() {
+	# send all of our output to stderr
+	exec 1>&2
+
+	# If arguments are given, redirect them to stdin this allows the funtion to be invoked with a string argument,
+	# or with stdin, e.g. via <<-EOF
+	(($#)) && exec <<<"$@"
+
+	echo
+	echo " !     WARNING: $(cat -)" | log::indent no_first_line_indent
+	echo
+}
+
+log::classic::warning_inline() {
+	echo " !     WARNING: $*" | log::indent no_first_line_indent
+}
+
+log::classic::status() {
+	echo "-----> $*"
+}
+
+log::classic::status_pending() {
+	echo -n "-----> $*..."
+}
+
+log::classic::status_done() {
+	echo " done"
+}
+
+log::classic::notice() {
+	echo
+	echo "NOTICE: $(cat -)" | log::indent
+	echo
+}
+
+log::classic::notice_inline() {
+	echo "NOTICE: $*" | log::indent
+}
+
+log::indent() {
+	# sed -l basically makes sed replace and buffer through stdin to stdout so you get updates while the command runs
+	# and dont wait for the end e.g. npm install | log::indent
+
+	# If any value (e.g. a non-empty string, or true, or false) is given for the first argument, this will act as a flag
+	# indicating we shouldn't indent the first line; we use :+ to tell SED accordingly if that parameter is set,
+	# otherwise null string for no range selector prefix (it selects from line 2 onwards and then every 1st line,
+	# meaning all lines)
+	#
+	# If the first argument is an empty string, it's the same as no argument (useful if a second argument is passed)
+	# the second argument is the prefix to use for indenting; defaults to seven space characters, but can be set to
+	# e.g. " !     " to decorate each line of an error message
+	local c="${1:+"2,999"} s/^/${2-"       "}/"
+
+	case $(uname) in
+	Darwin) sed -l "$c" ;; # mac/bsd sed: -l buffers on line boundaries
+	*) sed -u "$c" ;; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+	esac
+}
--- lib/maven.sh
+++ lib/maven.sh
@@ -1,3 +1,19 @@
+function maven::default_version() {
+	echo "3.6.2"
+}
+
+function maven::tarball_url_for_version() {
+	local -r maven_version="${1:?}"
+
+	declare -A maven_tarball_urls
+	maven_tarball_urls[3.6.2]="https://lang-jvm.s3.amazonaws.com/maven-3.6.2.tar.gz"
+	maven_tarball_urls[3.5.4]="https://lang-jvm.s3.amazonaws.com/maven-3.5.4.tar.gz"
+	maven_tarball_urls[3.3.9]="https://lang-jvm.s3.amazonaws.com/maven-3.3.9.tar.gz"
+	maven_tarball_urls[3.2.5]="https://lang-jvm.s3.amazonaws.com/maven-3.2.5.tar.gz"
+
+	echo "${maven_tarball_urls["${maven_version}"]}"
+}
+
 function maven::get_configured_version() {
 	local -r app_directory="${1:?}"
 	local -r default_version="${2}"
@@ -17,30 +33,14 @@ function maven::is_version_configured() {
 	[[ $(maven::get_configured_version "${app_directory}" "") != "" ]]
 }
 
-function maven::should_use_wrapper_for_app() {
-	local -r app_directory="${1:?}"
-
-	maven::app_contains_wrapper "${app_directory}" && ! maven::is_version_configured "${app_directory}"
-}
-
-function maven::tarball_url_for_version() {
-	local -r maven_version="${1:?}"
-
-	declare -A maven_tarball_urls
-	maven_tarball_urls[3.6.2]="https://lang-jvm.s3.amazonaws.com/maven-3.6.2.tar.gz"
-	maven_tarball_urls[3.5.4]="https://lang-jvm.s3.amazonaws.com/maven-3.5.4.tar.gz"
-	maven_tarball_urls[3.3.9]="https://lang-jvm.s3.amazonaws.com/maven-3.3.9.tar.gz"
-	maven_tarball_urls[3.2.5]="https://lang-jvm.s3.amazonaws.com/maven-3.2.5.tar.gz"
-
-	echo "${maven_tarball_urls["${maven_version}"]}"
-}
-
 function maven::app_contains_wrapper() {
 	local -r app_directory="${1:?}"
 
 	[[ -f "${app_directory}/mvnw" && -f "${app_directory}/.mvn/wrapper/maven-wrapper.properties" ]]
 }
 
-function maven::default_version() {
-	echo "3.6.2"
+function maven::should_use_wrapper_for_app() {
+	local -r app_directory="${1:?}"
+
+	maven::app_contains_wrapper "${app_directory}" && ! maven::is_version_configured "${app_directory}"
 }
