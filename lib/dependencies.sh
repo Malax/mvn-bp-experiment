6d3b1103714ccff68101ccb31cb1bfac10b2e2a0
Manuel Fuchs <manuel.fuchs@salesforce.com>
2020-11-23 14:18:45 +0100

    WIP

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -9,9 +9,10 @@ dependencies::has_spring_boot() {
 }
 
 dependencies::has_wildfly_swarm() {
-	local app_directory=${1:?}
-	[[ -f "${app_directory}/pom.xml" ]] &&
-		[[ -n "$(grep "<groupId>org.wildfly.swarm" "${app_directory}/pom.xml)")" ]]
+	#local app_directory=${1:?}
+	#[[ -f "${app_directory}/pom.xml" ]] &&
+	#	[[ -n "$(grep "<groupId>org.wildfly.swarm" "${app_directory}/pom.xml)")" ]]
+	false
 }
 
 dependencies::app_requires_postgres() {
